f4ebc0fafb5779da0ac7a848f22ccb52a332df16
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-08-14 13:22:01 +0100

    interface

---
+++ visibilies.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.21;
+
+contract Base {
+    // Private function can only be called
+    // - inside this contract
+    // Contracts that inherit this contract cannot call this function.
+    function privateFunc() private pure returns (string memory) {
+        return "private function called";
+    }
+
+    function testPrivateFuc() public pure returns (string memory) {
+        return privateFunc();
+    }
+
+    // Internal function can be called
+    // - inside this contract
+    // - inside contracts that inherit this contract
+    function internalFunc() internal pure returns (string memory) {
+        return "internal function called";
+    }
+
+    function testInternalFunc() public pure virtual returns (string memory) {
+        return internalFunc();
+    }
+    // State variables
+    string private privateVar = "my private variable";
+    string internal internalVar = "my internal variable";
+    string public publicVar = "my public variable";
+    // State variables cannot be external so this code won't compile.
+    // string external externalVar = "my external variable";
+}
+
+contract Child is Base {
+    // Inherited contracts do not have access to private functions
+    // and state variables.
+    // function testPrivateFunc() public pure returns (string memory) {
+    //     return privateFunc();
+    // }
+
+    // Internal function can be called inside child contracts.
+    function testInternalFunc() public pure override returns (string memory) {
+        return internalFunc();
+    }
+}
